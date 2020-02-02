6dc62bad148581d07555252e9cd1016c9526615f
MManoah <47468428+MManoah@users.noreply.github.com>
2020-02-02 13:39:16 -0500

    added time of purchase

--- Champion And Skin Grabber/Champion And Skin Grabber.py
+++ Champion And Skin Grabber/Champion And Skin Grabber.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from tkinter import *
 from tkinter import messagebox
 import psutil
@@ -15,6 +16,8 @@ class MainWindow:
     owned_champions = []
     owned_skins = []
     owned_skin_count = 0
+    skins_purchased_date = {}
+    champions_purchased_date = {}
 
     def __init__(self, master):
         master.iconbitmap('Icon.ico')
@@ -40,24 +43,38 @@ class MainWindow:
         skin_info.insert('end', self.owned_skin_count)
 
         champions = Button(master, text="Champion List", fg='#e6e6ff',
-                           bg='#0E0E0E', command=lambda: self.champion_window())
-        champions.place(relx=0.03, rely=0.83, relwidth=0.2, relheight=0.1)
+                           bg='#0E0E0E', command=lambda: self.champion_window(False))
+        champions.place(relx=0.03, rely=0.8, relwidth=0.2, relheight=0.1)
+        champions_date = Button(master, text="With Purchased Date", fg='#e6e6ff',
+                                bg='#0E0E0E', command=lambda: self.champion_window(True))
+        champions_date.place(relx=0.03, rely=0.91, relwidth=0.2, relheight=0.08)
 
         skins = Button(master, text="Skin List", fg='#e6e6ff',
-                       bg='#0E0E0E', command=lambda: self.skin_window())
-        skins.place(relx=0.77, rely=0.83, relwidth=0.2, relheight=0.1)
+                       bg='#0E0E0E', command=lambda: self.skin_window(False))
+        skins.place(relx=0.77, rely=0.8, relwidth=0.2, relheight=0.1)
+        skins_date = Button(master, text="With Purchased Date", fg='#e6e6ff',
+                            bg='#0E0E0E', command=lambda: self.skin_window(True))
+        skins_date.place(relx=0.77, rely=0.91, relwidth=0.2, relheight=0.08)
 
-    def champion_window(self):
-        championw = Toplevel(self.master)
-        ChampionsWindow(championw)
+    def champion_window(self, date):
+        if date:
+            championw = Toplevel(self.master)
+            ChampionsWindow(championw, True)
+        else:
+            championw = Toplevel(self.master)
+            ChampionsWindow(championw, False)
 
-    def skin_window(self):
-        skinw = Toplevel(self.master)
-        SkinsWindow(skinw)
+    def skin_window(self, date):
+        if date:
+            skinw = Toplevel(self.master)
+            SkinsWindow(skinw, True)
+        else:
+            skinw = Toplevel(self.master)
+            SkinsWindow(skinw, False)
 
 
 class SkinsWindow:
-    def __init__(self, master):
+    def __init__(self, master, date):
         self.master = master
         master.iconbitmap('Icon.ico')
         master.title('Skins')
@@ -75,13 +92,39 @@ class SkinsWindow:
         text['font'] = ('Courier', '12')
         text.place(relx=0.025, rely=0.02, relwidth=0.95, relheight=0.96)
         text.insert('end', 'To Copy Paste: Highlight all text then CTRL+C to copy and \nCTRL+V to paste\n\n')
-        text.insert('end', 'Skins:')
-        for x in MainWindow.owned_skins:
-            text.insert('end', '\n' + x)
+        text.insert('end', 'SKINS:\n')
+        if not date:
+            temp = MainWindow.owned_champions[0]
+            for x in range(len(MainWindow.owned_champions)):
+                owned = False
+                for i in range(len(MainWindow.owned_skins)):
+                    if temp in MainWindow.owned_skins[i]:
+                        text.insert('end', '\n' + MainWindow.owned_skins[i])
+                        owned = True
+                    else:
+                        temp = MainWindow.owned_champions[x]
+                if owned:
+                    text.insert('end', '\n')
+        else:
+            temp = MainWindow.owned_champions[0]
+            for x in range(len(MainWindow.owned_champions)):
+                owned = False
+                for i in range(len(MainWindow.owned_skins)):
+                    if temp in MainWindow.owned_skins[i]:
+                        text.insert('end', '\n' + MainWindow.owned_skins[i])
+                        text.insert('end', '\t\t\t -')
+                        text.insert('end', datetime.fromtimestamp(
+                            MainWindow.skins_purchased_date[MainWindow.owned_skins[i]] / 1000.0).strftime(
+                            '%B %d %Y %I:%M %p'))
+                        owned = True
+                    else:
+                        temp = MainWindow.owned_champions[x]
+                if owned:
+                    text.insert('end', '\n')
 
 
 class ChampionsWindow:
-    def __init__(self, master):
+    def __init__(self, master, date):
         self.master = master
         master.iconbitmap('Icon.ico')
         master.title('Champions')
@@ -99,9 +142,17 @@ class ChampionsWindow:
         text['font'] = ('Courier', '12')
         text.place(relx=0.025, rely=0.02, relwidth=0.95, relheight=0.96)
         text.insert('end', 'To Copy Paste: Highlight all text then CTRL+C to copy and \nCTRL+V to paste\n\n')
-        text.insert('end', 'CHAMPIONS:')
-        for x in MainWindow.owned_champions:
-            text.insert('end', '\n' + x)
+        text.insert('end', 'CHAMPIONS:\n')
+        if not date:
+            for x in MainWindow.owned_champions:
+                text.insert('end', '\n' + x)
+        else:
+            for x in range(len(MainWindow.owned_champions)):
+                text.insert('end', '\n' + MainWindow.owned_champions[x] + '\t\t -')
+                text.insert('end', datetime.fromtimestamp(
+                    MainWindow.champions_purchased_date[MainWindow.owned_champions[x]] / 1000.0).strftime(
+                    '%B %d %Y %I:%M %p'))
+                text.insert('end', '\n')
 
 
 def on_startup():
@@ -119,14 +170,20 @@ def on_startup():
                     MainWindow.owned_champion_count += 1
                     if champion_data[x]['alias'] == 'MonkeyKing':
                         MainWindow.owned_champions.append('Wukong')
+                        MainWindow.champions_purchased_date['Wukong'] = champion_data[x]['ownership']['rental'][
+                            'purchaseDate']
                     else:
                         MainWindow.owned_champions.append(champion_data[x]['alias'])
+                        MainWindow.champions_purchased_date[champion_data[x]['alias']] = \
+                            champion_data[x]['ownership']['rental']['purchaseDate']
             skin_get = await connector.request('get', '/lol-champions/v1/inventories/' + summoner + '/skins-minimal')
             skin_get = await skin_get.json()
             for x in range(len(skin_get)):
                 if skin_get[x]['ownership']['rental']['purchaseDate'] != 0:
                     MainWindow.owned_skin_count += 1
                     MainWindow.owned_skins.append(skin_get[x]['name'])
+                    MainWindow.skins_purchased_date[skin_get[x]['name']] = skin_get[x]['ownership']['rental'][
+                        'purchaseDate']
         else:
             exit(0)
 
@@ -150,4 +207,4 @@ if __name__ == '__main__':
         application = MainWindow(root)
         root.mainloop()
     else:
-        messagebox.showerror("ERROR", "Please login to the league client and try again")
+        messagebox.showerror("ERROR", "LEAGUE CLIENT IS NOT OPEN")
